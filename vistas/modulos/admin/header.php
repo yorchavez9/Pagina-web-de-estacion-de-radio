df321b0b029134498eec367eb16d407f9689af64
Jorge Chávez Huincho <djjmygm160399@gmail.com>
2024-03-15 08:30:37 -0500

    Modificando el header

--- vistas/modulos/admin/header.php
+++ vistas/modulos/admin/header.php
@@ -1,25 +1,26 @@
-<?php
-$item = null;
-$valor = null;
-
-$mensajesContacto = ControladorMensaje::ctrMostrarMensaje($item, $valor);
-
-$showState = 0;
-
-foreach ($mensajesContacto as $key => $value) {
-	if ($value["estado"] == 0) {
-		$showState++;
-	}
-}
-
-
-?>
 <header>
+	<?php
+	$item = null;
+	$valor = null;
+
+	$mensajesContacto = ControladorMensaje::ctrMostrarMensaje($item, $valor);
+
+	$showState = 0;
+
+	foreach ($mensajesContacto as $key => $value) {
+		if ($value["estado"] == 0) {
+			$showState++;
+		}
+	}
+	?>
 	<div class="topbar d-flex align-items-center">
 		<nav class="navbar navbar-expand gap-3">
 			<div class="mobile-toggle-menu"><i class='bx bx-menu'></i>
 			</div>
 
+			<div class="search-bar d-lg-block d-none" data-bs-toggle="modal" data-bs-target="#SearchModal">
+				<a href="avascript:;" class="btn d-flex align-items-center"><i class='bx bx-search'></i>Buscar</a>
+			</div>
 
 			<div class="top-menu ms-auto">
 				<ul class="navbar-nav align-items-center gap-1">
@@ -289,12 +290,13 @@ foreach ($mensajesContacto as $key => $value) {
 							<a href="javascript:;">
 								<div class="msg-header">
 									<p class="msg-header-title">Notificaciones</p>
-									<p class="msg-header-badge"><?php echo $showState; ?> Nuevos</p>
+									<p class="msg-header-badge"><?php echo $showState; ?> Nuevo</p>
 								</div>
 							</a>
 							<div class="header-notifications-list">
 								<?php
 								foreach ($mensajesContacto as $key => $value) {
+									if($value["estado"] == 0){
 								?>
 									<a class="dropdown-item" href="mensajeContacto">
 										<div class="d-flex align-items-center">
@@ -321,31 +323,219 @@ foreach ($mensajesContacto as $key => $value) {
 										</div>
 									</a>
 								<?php
+									}
 								}
 								?>
 							</div>
 							<a href="javascript:;">
 								<div class="text-center msg-footer">
-									<a href="mensajeContacto" class="btn btn-primary w-100">Ver todas las notificaciones</a>
+									<a href="mensajeContacto" class="btn btn-primary w-100">View All Notifications</a>
 								</div>
 							</a>
 						</div>
 					</li>
-
+					<li class="nav-item dropdown dropdown-large">
+						<a class="nav-link dropdown-toggle dropdown-toggle-nocaret position-relative" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"> <span class="alert-count">8</span>
+							<i class='bx bx-shopping-bag'></i>
+						</a>
+						<div class="dropdown-menu dropdown-menu-end">
+							<a href="javascript:;">
+								<div class="msg-header">
+									<p class="msg-header-title">My Cart</p>
+									<p class="msg-header-badge">10 Items</p>
+								</div>
+							</a>
+							<div class="header-message-list">
+								<a class="dropdown-item" href="javascript:;">
+									<div class="d-flex align-items-center gap-3">
+										<div class="position-relative">
+											<div class="cart-product rounded-circle bg-light">
+												<img src="vistas/dist/admin/assets/images/products/11.png" class="" alt="product image">
+											</div>
+										</div>
+										<div class="flex-grow-1">
+											<h6 class="cart-product-title mb-0">Men White T-Shirt</h6>
+											<p class="cart-product-price mb-0">1 X $29.00</p>
+										</div>
+										<div class="">
+											<p class="cart-price mb-0">$250</p>
+										</div>
+										<div class="cart-product-cancel"><i class="bx bx-x"></i>
+										</div>
+									</div>
+								</a>
+								<a class="dropdown-item" href="javascript:;">
+									<div class="d-flex align-items-center gap-3">
+										<div class="position-relative">
+											<div class="cart-product rounded-circle bg-light">
+												<img src="vistas/dist/admin/assets/images/products/02.png" class="" alt="product image">
+											</div>
+										</div>
+										<div class="flex-grow-1">
+											<h6 class="cart-product-title mb-0">Men White T-Shirt</h6>
+											<p class="cart-product-price mb-0">1 X $29.00</p>
+										</div>
+										<div class="">
+											<p class="cart-price mb-0">$250</p>
+										</div>
+										<div class="cart-product-cancel"><i class="bx bx-x"></i>
+										</div>
+									</div>
+								</a>
+								<a class="dropdown-item" href="javascript:;">
+									<div class="d-flex align-items-center gap-3">
+										<div class="position-relative">
+											<div class="cart-product rounded-circle bg-light">
+												<img src="vistas/dist/admin/assets/images/products/03.png" class="" alt="product image">
+											</div>
+										</div>
+										<div class="flex-grow-1">
+											<h6 class="cart-product-title mb-0">Men White T-Shirt</h6>
+											<p class="cart-product-price mb-0">1 X $29.00</p>
+										</div>
+										<div class="">
+											<p class="cart-price mb-0">$250</p>
+										</div>
+										<div class="cart-product-cancel"><i class="bx bx-x"></i>
+										</div>
+									</div>
+								</a>
+								<a class="dropdown-item" href="javascript:;">
+									<div class="d-flex align-items-center gap-3">
+										<div class="position-relative">
+											<div class="cart-product rounded-circle bg-light">
+												<img src="vistas/dist/admin/assets/images/products/04.png" class="" alt="product image">
+											</div>
+										</div>
+										<div class="flex-grow-1">
+											<h6 class="cart-product-title mb-0">Men White T-Shirt</h6>
+											<p class="cart-product-price mb-0">1 X $29.00</p>
+										</div>
+										<div class="">
+											<p class="cart-price mb-0">$250</p>
+										</div>
+										<div class="cart-product-cancel"><i class="bx bx-x"></i>
+										</div>
+									</div>
+								</a>
+								<a class="dropdown-item" href="javascript:;">
+									<div class="d-flex align-items-center gap-3">
+										<div class="position-relative">
+											<div class="cart-product rounded-circle bg-light">
+												<img src="vistas/dist/admin/assets/images/products/05.png" class="" alt="product image">
+											</div>
+										</div>
+										<div class="flex-grow-1">
+											<h6 class="cart-product-title mb-0">Men White T-Shirt</h6>
+											<p class="cart-product-price mb-0">1 X $29.00</p>
+										</div>
+										<div class="">
+											<p class="cart-price mb-0">$250</p>
+										</div>
+										<div class="cart-product-cancel"><i class="bx bx-x"></i>
+										</div>
+									</div>
+								</a>
+								<a class="dropdown-item" href="javascript:;">
+									<div class="d-flex align-items-center gap-3">
+										<div class="position-relative">
+											<div class="cart-product rounded-circle bg-light">
+												<img src="vistas/dist/admin/assets/images/products/06.png" class="" alt="product image">
+											</div>
+										</div>
+										<div class="flex-grow-1">
+											<h6 class="cart-product-title mb-0">Men White T-Shirt</h6>
+											<p class="cart-product-price mb-0">1 X $29.00</p>
+										</div>
+										<div class="">
+											<p class="cart-price mb-0">$250</p>
+										</div>
+										<div class="cart-product-cancel"><i class="bx bx-x"></i>
+										</div>
+									</div>
+								</a>
+								<a class="dropdown-item" href="javascript:;">
+									<div class="d-flex align-items-center gap-3">
+										<div class="position-relative">
+											<div class="cart-product rounded-circle bg-light">
+												<img src="vistas/dist/admin/assets/images/products/07.png" class="" alt="product image">
+											</div>
+										</div>
+										<div class="flex-grow-1">
+											<h6 class="cart-product-title mb-0">Men White T-Shirt</h6>
+											<p class="cart-product-price mb-0">1 X $29.00</p>
+										</div>
+										<div class="">
+											<p class="cart-price mb-0">$250</p>
+										</div>
+										<div class="cart-product-cancel"><i class="bx bx-x"></i>
+										</div>
+									</div>
+								</a>
+								<a class="dropdown-item" href="javascript:;">
+									<div class="d-flex align-items-center gap-3">
+										<div class="position-relative">
+											<div class="cart-product rounded-circle bg-light">
+												<img src="vistas/dist/admin/assets/images/products/08.png" class="" alt="product image">
+											</div>
+										</div>
+										<div class="flex-grow-1">
+											<h6 class="cart-product-title mb-0">Men White T-Shirt</h6>
+											<p class="cart-product-price mb-0">1 X $29.00</p>
+										</div>
+										<div class="">
+											<p class="cart-price mb-0">$250</p>
+										</div>
+										<div class="cart-product-cancel"><i class="bx bx-x"></i>
+										</div>
+									</div>
+								</a>
+								<a class="dropdown-item" href="javascript:;">
+									<div class="d-flex align-items-center gap-3">
+										<div class="position-relative">
+											<div class="cart-product rounded-circle bg-light">
+												<img src="vistas/dist/admin/assets/images/products/09.png" class="" alt="product image">
+											</div>
+										</div>
+										<div class="flex-grow-1">
+											<h6 class="cart-product-title mb-0">Men White T-Shirt</h6>
+											<p class="cart-product-price mb-0">1 X $29.00</p>
+										</div>
+										<div class="">
+											<p class="cart-price mb-0">$250</p>
+										</div>
+										<div class="cart-product-cancel"><i class="bx bx-x"></i>
+										</div>
+									</div>
+								</a>
+							</div>
+							<a href="javascript:;">
+								<div class="text-center msg-footer">
+									<div class="d-flex align-items-center justify-content-between mb-3">
+										<h5 class="mb-0">Total</h5>
+										<h5 class="mb-0 ms-auto">$489.00</h5>
+									</div>
+									<button class="btn btn-primary w-100">Checkout</button>
+								</div>
+							</a>
+						</div>
+					</li>
 				</ul>
 			</div>
 			<div class="user-box dropdown px-3">
 				<a class="d-flex align-items-center nav-link dropdown-toggle gap-3 dropdown-toggle-nocaret" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
 					<img src="vistas/dist/admin/assets/images/avatars/avatar-2.png" class="user-img" alt="user avatar">
 					<div class="user-info">
-						<p class="user-name mb-0"><?php echo $_SESSION["nombre"] ?></p>
-						<p class="designattion mb-0"><?php echo $_SESSION["perfil"] ?></p>
+						<p class="user-name mb-0"><?php echo $_SESSION["nombre"]?></p>
+						<p class="designattion mb-0"><?php echo $_SESSION["perfil"]?></p>
 					</div>
 				</a>
 				<ul class="dropdown-menu dropdown-menu-end">
-					<li><a class="dropdown-item d-flex align-items-center" href="javascript:;"><i class="bx bx-user fs-5"></i><span>Perfil</span></a>
+					<li><a class="dropdown-item d-flex align-items-center" href="usuarios"><i class="bx bx-user fs-5"></i><span>Perfil</span></a>
 					</li>
 					<li><a class="dropdown-item d-flex align-items-center" href="javascript:;"><i class="bx bx-cog fs-5"></i><span>Configuración</span></a>
+					</li>
+					<li><a class="dropdown-item d-flex align-items-center" href="Inicio"><i class="bx bx-home-circle fs-5"></i><span>Inicio</span></a>
 					</li>
 					<li>
 						<div class="dropdown-divider mb-0"></div>
